02e3e71525bdb70d8c4b6a1eba38922e9c478fbb
Akash Srivastava <akash221002@gmail.com>
2022-10-02 14:52:37 +0530

    Stated Project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9x2ZK0G7ag9MDiEr0Q1vaZ3YCDUyI2sA",
+  authDomain: "netflix-clone-53b82.firebaseapp.com",
+  projectId: "netflix-clone-53b82",
+  storageBucket: "netflix-clone-53b82.appspot.com",
+  messagingSenderId: "672808463394",
+  appId: "1:672808463394:web:5d4bef20935ce42ba6ee7b",
+};
