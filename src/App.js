52262b338b383f4aa7db35fc83d9a3c306fa6f02
Akash Srivastava <akash221002@gmail.com>
2022-10-14 18:38:33 +0530

    Implemented login and logout functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import HomeScreen from "./pages/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/UserSlice";
 
 function App() {
-  const user = true;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
+        dispatch(logout);
       }
     });
     return unsubscribe;
